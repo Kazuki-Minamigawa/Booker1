3af788406766e47c9c8f399ea6de27632a78cf8f
Kazuki-Ninamigawa <minamigawa.kazuki.infratop@gmail.com>
2020-03-03 18:38:08 +0900

    [Finish]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:create] = "Book was successfully created."
+      redirect_to "/books/#{@book.id}"
+    else
+      render "index"
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:update] = "Book was successfully updated."
+      redirect_to "/books/#{book.id}"
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:destroy] = "Book was successfully destroyed."
+      redirect_to "/books"
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "books#top"
+  get "/books" => "books#index"
+  post "/books" => "books#create"
+  get "/books/:id" => "books#show"
+  get "/books/:id/edit" => "books#edit"
+  patch "/books/:id" => "books#update"
+  delete "/books/:id" => "books#destroy"
+end
