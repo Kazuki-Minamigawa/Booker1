4443a9f15e96ad31c953e65507800de6a6ad78b6
Kazuki-Ninamigawa <minamigawa.kazuki.infratop@gmail.com>
2020-03-03 22:48:53 +0900

    [Update]

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_164856) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "category"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
